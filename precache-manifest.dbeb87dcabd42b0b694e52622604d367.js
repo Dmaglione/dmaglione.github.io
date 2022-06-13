fb7f327b4d1f4b8f65034ec03efec5e7bc4b4d7e
Dmaglione <Dmag171@gmail.com>
2022-06-13 12:48:45 -0400

    Updates

---
+++ precache-manifest.dbeb87dcabd42b0b694e52622604d367.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa239238b44c573d7db1b5c1444a093d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3144731793601cacc6f",
+    "url": "/static/css/main.0e2f8416.chunk.css"
+  },
+  {
+    "revision": "d89c7aea150910751cd5",
+    "url": "/static/js/2.e8b4d312.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e8b4d312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3144731793601cacc6f",
+    "url": "/static/js/main.a4bdbe04.chunk.js"
+  },
+  {
+    "revision": "8742829eb23446476a4b",
+    "url": "/static/js/runtime-main.9dbcde2c.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
